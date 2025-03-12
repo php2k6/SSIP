d0a413637c6eed0cb92918b4194bee9551ae2541
Prabhav Patel <php2k6@gmail.com>
2025-03-12 10:40:00 +0530

    Added test.py

---
+++ test.py
@@ -0,0 +1,18 @@
+from g4f.client import Client
+import g4f
+import g4f.Provider
+from ocr_extraction import ocr_extraction
+from text_cleanup import clean_text
+client = Client()
+#PARAMETERS
+test_provider = g4f.Provider.Yqcloud
+test_model = "gpt-4"
+test_prompt = "hello"
+
+response = client.chat.completions.create(
+    model=test_model,
+    messages=[{"role": "user", "content": test_prompt}],
+    web_search=False,
+    provider=test_provider,
+)
+print(response.choices[0].message.content)
